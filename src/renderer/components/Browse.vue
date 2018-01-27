1f2876079c3f95964071018d94eaf29f806fa4e6
liquid8d <liquid8d@gmail.com>
2018-01-27 01:31:58 -0500

    new style, pull from remote content

--- src/renderer/components/Browse.vue
+++ src/renderer/components/Browse.vue
@@ -2,26 +2,56 @@
     <div class="page">
         <div class="container">
             <h2>Browse</h2>
-            <button @click="play('http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4')">MP4</button>
-            <button @click="play('http://dash.edgesuite.net/akamai/test/caption_test/ElephantsDream/elephants_dream_480p_heaac5_1.mpd')">DASH</button>
-            <button @click="play('https://www.youtube.com/watch?v=8ndhidEmUbI')">YT</button>
-            <button @click="play('https://s3.amazonaws.com/_bc_dml/example-content/bipbop-advanced/bipbop_16x9_variant.m3u8')">HLS</button>
-            <button @click="play('http://cdn3.videos.bloomberg.com/btv/us/master.m3u8')">HLS Live</button>
-            <button @click="play('https://vimeo.com/channels/360vr/207528129')">Vimeo360</button>
-            <button @click="play('rtmp://cp67126.edgefcs.net/ondemand/mediapm/osmf/content/test/akamai_10_year_f8_512K')">RTMP (requires flash)</button>
+        </div>
+        <div class="container" style="flex-grow:1; overflow:auto;">
+            <div v-if="items" class="card" v-bind:key="item.title" v-for="item in items">
+                <img v-bind:src="item.poster" />
+                <div class="content">
+                    <h1>{{item.title}}</h1>
+                    <p>{{item.desc}}</p>
+                    <button @click.prevent="play(item.src)">Play</button>
+                </div>
+            </div>
         </div>
         <div class="container">
             <router-link to="/" tag="button">Back</router-link>
+            <!-- <button @click.prevent="fetch('https://goodiesfor.me/bbviewer/content')">Fetch</button> -->
         </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     const { PlayerEvents } = require('./Player/PlayerEvents')
+    
     export default {
         name: 'browse',
         components: { PlayerEvents },
+        data () {
+            return {
+                lastFetch: 0,
+                updateFreq: 300,
+                items: []
+            }
+        },
+        mounted () {
+            // do fetch
+            if (this.items.length === 0 || Date.now() > this.lastFetch + this.updateFreq) {
+                this.updateFreq = Date.now()
+                this.fetch('http://goodiesfor.me/bbviewer/content')
+            }
+            this.$extendedInput.selectEl()
+        },
         methods: {
+            fetch (src) {
+                axios.get(src)
+                    .then((response) => {
+                        if (response.data && response.data.items) this.items = response.data.items
+                    })
+                    .catch((err) => {
+                        console.log('error getting content: ' + err)
+                    })
+            },
             play (src) {
                 this.playerRedirect('play', src)
                 this.$router.push('/')
@@ -32,3 +62,91 @@
         }
     }
 </script>
+
+<style>
+    .card {
+        position: relative;
+        display: inline-flex;
+        background-color: #111;
+        border: 2px solid #222;
+        margin: 1em;
+        padding: 0.5em;
+        width: 10em;
+        height: 12em;
+    }
+
+    .card > img {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .card > .content {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 4em;
+        padding: 0.5em;
+        background-color: rgba(10, 10, 10, 0.9);
+    }
+
+    .card > .content > h1 {
+        color: #efefef;
+        font-size: 0.75em;
+    }
+
+    .card > .content > p {
+        color: #888;
+        font-size: 0.6em;
+    }
+
+    .card button {
+        position: absolute;
+        bottom: 2.5em;
+        right: 0;
+    }
+
+    .spinner {
+    margin: 100px auto 0;
+    width: 70px;
+    text-align: center;
+    }
+
+    .spinner > div {
+    width: 18px;
+    height: 18px;
+    background-color: #333;
+
+    border-radius: 100%;
+    display: inline-block;
+    -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+    animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+    }
+
+    .spinner .bounce1 {
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+    }
+
+    .spinner .bounce2 {
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+    }
+
+    @-webkit-keyframes sk-bouncedelay {
+    0%, 80%, 100% { -webkit-transform: scale(0) }
+    40% { -webkit-transform: scale(1.0) }
+    }
+
+    @keyframes sk-bouncedelay {
+    0%, 80%, 100% { 
+        -webkit-transform: scale(0);
+        transform: scale(0);
+    } 40% { 
+        -webkit-transform: scale(1.0);
+        transform: scale(1.0);
+    }
+    }
+</style>

da820be076a083055b01e5f74f5f3571ced4a4c0
liquid8d <liquid8d@gmail.com>
2018-01-31 09:59:05 -0500

    add donate view, new routes

---
+++ src/renderer/components/Donate.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="page">
+        <div class="container">
+            <h1>{{ $t('title') }}</h1>
+        </div>
+        <div class="container">
+            <webview ref="browser" v-if="$store.state.isElectron" :src="url"></webview>
+            <div v-else class="browser-donate">
+                <h1>{{ $t('label') }}</h1>
+                <p>{{ $t('desc') }}</p>
+                <a :href="url" target="_blank">{{ $t('link') }}</a>
+            </div>
+        </div>
+        <div class="container">
+            <router-link to="/" tag="button">{{ $t('back') }}</router-link>
+        </div>
+    </div>
+</template>
+
+<i18n>
+{
+    "en": {
+        "title": "Donate",
+        "label": "Thank you for your support!",
+        "desc": "You can donate to help support the development of BBViewer via PayPal by clicking the link below.",
+        "link": "Click here to donate!"
+    }
+}
+</i18n>
+
+<style scoped>
+    webview {
+        width: 100%;
+    }
+</style>
+
+<script>
+    export default {
+        data () {
+            return {
+                url: 'https://www.paypal.me/liquid8d'
+            }
+        },
+        methods: {
+            close () {
+                this.$router.replace('/')
+            }
+        }
+    }
+</script>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -8,6 +8,8 @@ export default new Router({
         { path: '/', name: 'home', component: require('@/components/Home').default },
         { path: '/about', name: 'about', component: require('@/components/About').default },
         { path: '/browse', name: 'browse', component: require('@/components/Browse').default },
+        { path: '/bookmarks', name: 'bookmarks', component: require('@/components/Bookmarks').default },
+        { path: '/donate', name: 'donate', component: require('@/components/Donate').default },
         { path: '/dev', name: 'dev', component: require('@/components/Dev').default },
         { path: '/settings', name: 'settings', component: require('@/components/Settings').default },
         { path: '*', redirect: '/' }
